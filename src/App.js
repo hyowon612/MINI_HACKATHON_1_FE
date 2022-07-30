80953e1763225e66ad32d2a3a70bd32057f689c4
Yebin Kim <rladpqls07@gmail.com>
2022-07-30 11:21:32 +0900

    feat: Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/shared/header';
 import Template from './components/template';
 
 const App = () => {
   return (
     <Template>
-      
+      <Header/>
     </Template>
   )
 }
--- src/components/shared/header/styled.js
+++ src/components/shared/header/styled.js
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 
 export const HeaderSection = styled.div`
+  margin: 0 auto;
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .isHide {
+    display: none;
+  }
+  .isShow {
+    display: block;
+  }
+`;
 
+export const HeaderLogo = styled.h2``;
+export const HeaderBtnBlock = styled.div``;
+export const HeaderBtn = styled.button`
+  background-color: inherit;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.7;
+  }
 `;
