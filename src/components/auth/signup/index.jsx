664375576a559353f30482751e0e960fd82eefd5
Hyowon Jeon <hyowon612@gmail.com>
2022-07-31 11:32:23 +0900

    auth service 추가

--- src/components/auth/signup/index.jsx
+++ src/components/auth/signup/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   SignUpSection, 
   SignUpComponent,
@@ -7,21 +7,55 @@ import {
   PasswordInput, 
   SignUpButton
 } from "./styled";
+import AuthService from "../../../apis/auth-service";
+import { useNavigate } from 'react-router-dom';
+
+const authService = new AuthService();
 
 const SignUp = () => {
+
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const result = await authService.signup(username, password);
+      console.log(result);
+      console.log("회원가입버튼");
+      if(true) {
+        navigate(`/`);
+      };
+      return;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <SignUpSection>
       <SignUpComponent>
         <Title>회원가입</Title>
         <IdInput
+          onChange={handleUsername}
           type="text" 
           placeholder="아이디"
         />
         <PasswordInput
+          onChange={handlePassword}
           type="text" 
           placeholder="비밀번호"
         />
-        <SignUpButton>회원가입</SignUpButton>
+        <SignUpButton onClick={handleSubmit}>회원가입</SignUpButton>
       </SignUpComponent>
     </SignUpSection>   
   )
